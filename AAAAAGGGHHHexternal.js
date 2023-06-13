6ce03ae730e95baaf36391e683703b87a840a79b
jazilbazil <jazjazzle@icloud.com>
2023-06-14 09:24:44 +1000

    ,jnmk

--- AAAAAGGGHHHexternal.js
+++ AAAAAGGGHHHexternal.js
@@ -3,9 +3,29 @@ function appear()
     document.getElementById('myImg').style.display = 'block';
 
     let myRed = document.getElementById("mycolor").value;
-    let myBlue = document.getElementById("mycolour").value;
-    let myGreen = document.getElementById("mycolur").value;
+    let myBlue = document.getElementById("mycolon").value;
+    let myGreen = document.getElementById("mycola").value;
+    
+    if(myRed > 255)
+    {
+        document.getElementById('mycolor').value = 255;
+        myRed = 255;
+    }
+    if(myBlue > 255)
+    {
+        document.getElementById('mycolon').value = 255;
+        myBlue = 255;
+    }
+    if(myGreen > 255)
+    {
+        document.getElementById('mycola').value = 255;
+        myGreen = 255;
+    }
+    
+    else{
+        document.getElementById('tester').style.backgroundColor = `rgb(${myRed}, ${myBlue}, ${myGreen})`;
+
+    }
 
 
-    document.getElementById('tester').backgroundColor = `rgb(${myRed}, ${myBlue}, ${myGreen})`;
 }
